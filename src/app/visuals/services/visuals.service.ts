06c41db19a71852fa4f40c5c646ed180530ca384
equii <theequimanthorn@gmail.com>
2019-12-16 23:59:30 +0200

    fullscreen mode + new visuals

--- src/app/visuals/services/visuals.service.ts
+++ src/app/visuals/services/visuals.service.ts
@@ -3,87 +3,125 @@ import * as p5 from 'p5';
 import { Output, EventEmitter } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class VisualsService {
 	private started = false;
 	@Output() loaded = new EventEmitter();
 
-  constructor() {
-		let self = this;
-		let s = sk => {
-			sk.setup = () => {
-				var cols, rows;
-				var scl = 20;
+	constructor() {
+		let sketch = sketchFun;
 
-				var physicalScreenWidth = window.screen.width * window.devicePixelRatio;
-				var physicalScreenHeight = window.screen.height * window.devicePixelRatio;
+		function sketchFun(sk) {
+			let sequences = [];
+			let speed = 0.003;
+			let scale = 33;
+			let doClear = false;
+			let doFade = true;
+			let numSequences = 200;
+			let pi = Math.PI;
+			let alpha = 6;
+			let centerX, centerY;
 
-				var w = physicalScreenWidth;
-				var h = physicalScreenHeight;
+			function setup() {
+				// let physicalScreenWidth = window.screen.width * window.devicePixelRatio;
+				// let physicalScreenHeight = window.screen.height * window.devicePixelRatio;
+	
+				// let w = physicalScreenWidth;
+				// let h = physicalScreenHeight;
+				sk.createCanvas(window.screen.width, window.screen.height);
 
-				console.log(w);
-				console.log(h);
-		
-				var flying = 0;
-		
-				var terrain = [];
-		
-				sk.createCanvas(window.screen.height, window.screen.width, sk.WEBGL);
-				cols = w / scl;
-				rows = h / scl;
-		
-				for (var x = 0; x < cols; x++) {
-					terrain[x] = [];
-					for (var y = 0; y < rows; y++) {
-						terrain[x][y] = 0; //specify a default value for now
+				centerX = sk.width / 2;
+				centerY = sk.height / 2;
+
+				sk.blendMode(sk.BLEND);
+
+				scale = sk.width / 20;
+				sk.background(0);
+				_setupSequence();
+			}
+
+			// function mouseWheel(event) {
+			// 	if (event.delta < 0) {
+			// 		alpha += 5;
+			// 	} else {
+			// 		alpha -= 5;
+			// 	}
+
+			// 	console.log(alpha);
+			// }
+			sk.draw = draw;
+			sk.setup = setup;
+
+			function draw() {
+				let startAtX = 0; //-10;
+				let startAtY = sk.height; //height;
+
+				if (doClear) sk.background(0);
+				if (doFade) sk.background(0, alpha + 10);
+
+				for (let i = 1; i < sequences.length; i++) {
+					let sequence = sequences[i];
+
+					let len = sk.noise(sk.frameCount * speed, sk.frameCount * speed) * scale;
+					let angle = sk.noise(sk.frameCount * speed, 2*pi + sk.frameCount * speed); 
+
+					sk.resetMatrix();
+					sk.translate(startAtX, startAtY);
+
+					let mult = -1;
+					if (sk.random(1) > 0.2) {
+						mult = -1;
+					}
+
+					for (let j = 0; j < sequence.length; j++) {
+						let value = sequence[j];
+						if (value % 2 == 0) {
+							sk.rotate(angle);
+						} else {
+							sk.rotate(-angle);
+						}
+						sk.strokeWeight(1);
+
+						sk.stroke(255, alpha);
+
+						sk.line(0, 0, 0, len * mult);
+						sk.translate(0, len * mult);
 					}
 				}
+			}
 
-				function draw() {
-					if(!self.started) {
-						return;
-					}
+			function _setupSequence() {
+				sequences = [];
+				for (let i = 1; i < numSequences; i++) {
+					let sequence = [];
 
-					flying -= 0.1;
-					var yoff = flying;
-					for (var y = 0; y < rows; y++) {
-						var xoff = 0;
-						for (var x = 0; x < cols; x++) {
-							terrain[x][y] = sk.map(sk.noise(xoff, yoff), 0, 1, -100, 100);
-							xoff += 0.2;
-						}
-						yoff += 0.2;
-					}
-		
-					sk.background(0);
-					sk.translate(0, 50);
-					sk.rotateX(sk.PI / 3);
-					sk.fill(200, 200, 200, 50);
-					sk.translate(-w / 2, -h / 2);
-					for (var y = 0; y < rows - 1; y++) {
-						sk.beginShape(sk.TRIANGLE_STRIP);
-						for (var x = 0; x < cols; x++) {
-							sk.vertex(x * scl, y * scl, terrain[x][y]);
-							sk.vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
-						}
-						sk.endShape();
-					}
+					let n = i;
+					do {
+						sequence.push(n);
+						n = _collatz(n);
+					} while (n != 1);
+
+					sequence.push(1);
+					sequence.reverse();
+
+					sequences.push(sequence);
 				}
-		
-				sk.draw = draw;
+			}
 
-				setTimeout(()=> {
-					console.log('1visuals loaded')
-					self.loaded.emit('visuals')
-				})
-			};
-		};
-		
-		const P5 = new p5(s, 'canvas-holder');
-  }
+			function _collatz(n) {
+				if (n % 2 == 0) {
+					return n / 2;
+				} else {
+					return (n * 3 + 1) / 2;
+				}
+			}
+		}
 
-  start(): void {
+		const P5 = new p5(sketch, 'canvas-holder');
+	}
+
+	start(): void {
 		this.started = true;
-  }
+	}
 }
